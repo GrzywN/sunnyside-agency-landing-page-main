259eb6ebaea84f0c1be56fa240ba44e8bb73abf6
Karol Binkowski <karolbinkowski3@protonmail.com>
2022-04-17 23:14:59 +0200

    Made, so all events in JS are handled using event delegation
    Also I think it fixed some problems with Chrome browser

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,20 +4,23 @@ const hamburger = document.getElementById('hamburger');
 let isMenuExpanded = false;
 let resizeTimer;
 
+const handleUserClicks = (event) => {
+  if (isClickedOnHamburger(event)) {
+    toggleNavMenu();
+  } else if (isMenuExpanded && isClickedOutsideMenu(event)) {
+    toggleNavMenu();
+  }
+};
+
+const isClickedOnHamburger = (event) => event.target.id === 'hamburger';
+const isClickedOutsideMenu = (event) => !event.target.classList.contains('nav__list');
+
 const toggleNavMenu = () => {
   isMenuExpanded = !isMenuExpanded;
   hamburger.dataset.expanded = `${isMenuExpanded}`;
   hamburger.ariaExpanded = isMenuExpanded;
   nav.dataset.expanded = `${isMenuExpanded}`;
 };
-
-const closeWhileClickedOutside = (event) => {
-  if (!isMenuExpanded) return;
-  if (isClickedOutside(event)) toggleNavMenu();
-};
-
-const isClickedOutside = (event) =>
-  !event.target.classList.contains('nav__list') && event.target.id !== 'hamburger';
 
 const stopAnimationOnResize = () => {
   document.body.classList.add('resize-animation-stopper');
@@ -27,7 +30,5 @@ const stopAnimationOnResize = () => {
   }, 400);
 };
 
-hamburger.addEventListener('click', toggleNavMenu);
-document.addEventListener('click', (event) => closeWhileClickedOutside(event));
-
+document.addEventListener('click', (event) => handleUserClicks(event));
 window.addEventListener('resize', stopAnimationOnResize);
